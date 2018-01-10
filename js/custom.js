2a149f6370eb467a40034a41c2fc160ee826d238
khanhhbvip1 <khanhhbvip1@gmail.com>
2018-01-10 00:46:41 -0700

    first commit

---
+++ js/custom.js
@@ -0,0 +1,115 @@
+// menu
+(function($) {
+    $.fn.menumaker = function(options) {
+        var cssmenu = $(this),
+            settings = $.extend({
+                format: "dropdown",
+                sticky: false
+            }, options);
+        return this.each(function() {
+            $(this).find(".button").on('click', function() {
+                $(this).toggleClass('menu-opened');
+                var mainmenu = $(this).next('ul');
+                if (mainmenu.hasClass('open')) {
+                    mainmenu.slideToggle().removeClass('open');
+                } else {
+                    mainmenu.slideToggle().addClass('open');
+                    if (settings.format === "dropdown") {
+                        mainmenu.find('ul').show();
+                    }
+                }
+            });
+            cssmenu.find('li ul').parent().addClass('has-sub');
+            multiTg = function() {
+                cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
+                cssmenu.find('.submenu-button').on('click', function() {
+                    $(this).toggleClass('submenu-opened');
+                    if ($(this).siblings('ul').hasClass('open')) {
+                        $(this).siblings('ul').removeClass('open').slideToggle();
+                    } else {
+                        $(this).siblings('ul').addClass('open').slideToggle();
+                    }
+                });
+            };
+            if (settings.format === 'multitoggle') multiTg();
+            else cssmenu.addClass('dropdown');
+            if (settings.sticky === true) cssmenu.css('position', 'fixed');
+            resizeFix = function() {
+                var mediasize = 1000;
+                if ($(window).width() > mediasize) {
+                    cssmenu.find('ul').show();
+                }
+                if ($(window).width() <= mediasize) {
+                    cssmenu.find('ul').hide().removeClass('open');
+                }
+            };
+            resizeFix();
+            return $(window).on('resize', resizeFix);
+        });
+    };
+})(jQuery);
+
+(function($) {
+    $(document).ready(function() {
+        $("#cssmenu").menumaker({
+            format: "multitoggle"
+        });
+    });
+})(jQuery);
+// end menu
+// thư viện sản phẩm
+
+
+// scrtroll
+$(function() {
+    $(window).scroll(function(event) {
+        //console.log('bạn vừa cuộn chuột');
+        //console.log($('html,body').scrollTop());
+        var vitri = $('html,body').scrollTop();
+        if (vitri >= 150) {
+            //console.log('ahihi');
+            $('.goidv').addClass('chuyendong');
+        } else {
+            $('.goidv').removeClass('chuyendong');
+        }
+        if (vitri >= 500) {
+            $('.hotting').addClass('chuyendong1');
+        } else {
+            $('.hotting').removeClass('chuyendong1');
+        }
+      
+    });
+});
+
+$(function() {
+    // browser window scroll (in pixels) after which the "back to top" link is shown
+    var offset = 300,
+        //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+        offset_opacity = 1200,
+        //duration of the top scrolling animation (in ms)
+        scroll_top_duration = 700,
+        //grab the "back to top" link
+        $back_to_top = $('.cd-top');
+
+    //hide or show the "back to top" link
+    $(window).scroll(function() {
+        ($(this).scrollTop() > offset) ? $back_to_top.addClass('cd-is-visible'): $back_to_top.removeClass('cd-is-visible cd-fade-out');
+        if ($(this).scrollTop() > offset_opacity) {
+            $back_to_top.addClass('cd-fade-out');
+        }
+    });
+
+    //smooth scroll to top
+    $back_to_top.on('click', function(event) {
+        event.preventDefault();
+        $('body,html').animate({
+            scrollTop: 0,
+        }, scroll_top_duration);
+    });
+
+});
+
+$(window).load(function() {
+    $('body').removeClass('preloading');
+    $('#preload').delay(1000).fadeOut('fast');
+});
